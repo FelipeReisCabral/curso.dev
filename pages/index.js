b7687f58f4db2135d50df246e36ad2bcc18786ea
Felipe Cabral <145393937+FelipeReisCabral@users.noreply.github.com>
2024-09-23 14:38:43 +0000

    adicionado pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>TESTE 3</h1>
+    return <h1> <p>"Há 24 anos, encontrei em você não apenas o amor, mas o meu lar, o meu abrigo, o meu tudo. <br />
+    Como disse Vinicius de Moraes, 'que não seja imortal, posto que é chama, mas que seja infinito enquanto dure'.<br />
+     E que esse infinito continue a nos guiar, com o brilho de cada momento que compartilhamos."</p> </h1>
 }
 
 export default Home;
